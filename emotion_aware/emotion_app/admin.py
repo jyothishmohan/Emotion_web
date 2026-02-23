65d4a1c475e06e5033f77b4dd5ada9fe737fe6f4
jyothishmohan <156563567+jyothishmohan@users.noreply.github.com>
2026-02-23 10:57:47 +0530

    backend: comment changed on admin.py

--- emotion_aware/emotion_app/admin.py
+++ emotion_aware/emotion_app/admin.py
@@ -5,5 +5,7 @@ class EmotionResultAdmin(admin.ModelAdmin):
     list_display = ('user', 'emotion', 'created_at')
     list_filter = ('emotion', 'created_at')
     search_fields = ('user__username', 'emotion')
+    
+    
 
 admin.site.register(EmotionResult, EmotionResultAdmin)
